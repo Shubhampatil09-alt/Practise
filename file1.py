c6a1630ddf4595f5bc0c54ec1ca027c6c7039b18
Shubhampatil09-alt <patil.shubham222@gmail.com>
2026-02-04 00:48:55 +0530

    third commit

--- file1.py
+++ file1.py
@@ -1,1 +1,20 @@
-print("Hello Shubham Patil")
+# for loop
+"""_summary_
+For loop is used for iterating over a sequnce (i.e. either a list, tuple, dictionary or set)
+With for loop we can execute a set of statement.
+- With "Continue" we can stop curent iteration of for loop and continue with next.
+Range function.
+- Renge function return a sequence of number, starting from 0 by Default, and increament by 1(By default)
+however it is possible to specify increament value by adding third variable
+# Else in for loop.
+The Else keyword in for loop specify block of code, to be executed when loop is finished.
+
+Nested loop: 
+Nested loop is loop inside a loop.
+the inner loop will executed one time for each iteration of the outer loop.
+"""
+
+lst = ["A", "B","C","D"]
+for i in lst:
+    print(i)
+
