8e2151d0dc3a4bac1dfa0b6559845a4d171385fd
Shubhampatil09-alt <patil.shubham222@gmail.com>
2026-02-09 23:28:27 +0530

    Tempterature conversion

--- Weight_convertor.py
+++ Weight_convertor.py
@@ -1,14 +1,28 @@
-# Python weight convertor.
-wt = float(input("Enter your Weight: "))
-unit = input("Kilogram or pound ? ( K Or L): ").upper()
+# # Python weight convertor.
+# wt = float(input("Enter your Weight: "))
+# unit = input("Kilogram or pound ? ( K Or L): ").upper()
 
-if unit == "K":
-    wt = wt*2.205
-    unit = "Lbs."
-elif unit =="L":
-    wt = wt / 2.205
-    unit = "Kgs"
-else:
-    print(f" {unit} was not valid..... ")
+# if unit == "K":
+#     wt = wt*2.205
+#     unit = "Lbs."
+# elif unit =="L":
+#     wt = wt / 2.205
+#     unit = "Kgs"
+# else:
+#     print(f" {unit} was not valid..... ")
     
-print(f" Your weight is {wt} {unit}")    
+# print(f" Your weight is {wt} {unit}")    
+#==========================================================================
+# Temperatur conversion program.
+
+unit =  input("Enter Unit (C/F): ").upper()
+temp = float(input("Enter the Temp: "))
+if unit == "C":
+    temp = round((9*temp)/ 5+32,1)
+    print(f"Temperature in Farhenit:  {temp} C")
+elif unit == "F":
+    temp = round((temp - 32) *5/9,1)
+    print(f"Temperature in Celsius:  {temp} F")
+else:
+    print(f"{unit} is invalid of measure")
+print("")
