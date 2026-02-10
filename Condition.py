7f1ba0d47d45ebd442f42dc4872b2a256c1af999
Shubhampatil09-alt <patil.shubham222@gmail.com>
2026-02-11 00:56:13 +0530

    One liner if else statment

---
+++ Condition.py
@@ -0,0 +1,18 @@
+# conditional expression = A one line shortcut for If-else statment.
+# x if condition else Y
+
+num = 6
+a=5
+b=5
+Age = 15
+Temp = 30
+
+# print("Positive" if num> 0 else "Negative") 
+# print("Even" if num %2==0 else "ODD")
+# print("greate number is A" if a > b else "Greater number is B")
+# print("Smaller number is A" if a < b else "Greater number is B")
+# print("Smaller number is A" if a < b else "Greater number is B")
+# rslt= "Adult" if Age >18  else "Child"
+rslt= "It's HOT" if Temp >20  else "It's COLD"
+
+print(rslt)
